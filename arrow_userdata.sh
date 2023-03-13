13533187e8f59d0c4a784ea1012ada6ea6c748cd
arrow <alparslanu6347@gmail.com>
2023-03-13 09:33:43 +0300

    userdata updated

--- arrow_userdata.sh
+++ arrow_userdata.sh
@@ -1,27 +1,12 @@
-# #!/bin/bash 
-# yum update -y
-# yum install python3 -y
-# pip3 install flask
-# cd /home/ec2-user
-# wget https://raw.github.com/alparslanu6347/roman-numerals_terraform/app.py
-# mkdir templates
-# cd templates
-# wget https://raw.github.com/alparslanu6347/roman-numerals_terraform/templates/index.html
-# wget https://raw.github.com/alparslanu6347/roman-numerals_terraform/templates/result.html
-# cd ..
-# python3 app.py  
-
-
 #! /bin/bash
 yum update -y
 yum install python3 -y
 pip3 install flask
 yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.github.com/alparslanu6347/roman-numerals_terraform/templates/index.html
-wget -P templates https://raw.github.com/alparslanu6347/roman-numerals_terraform/templates/result.html
-wget https://raw.github.com/alparslanu6347/roman-numerals_terraform/app.py
+wget -P templates https://raw.githubusercontent.com/alparslanu6347/roman-numerals_terraform/main/templates/index.html
+wget -P templates https://raw.githubusercontent.com/alparslanu6347/roman-numerals_terraform/main/templates/result.html
+wget https://raw.githubusercontent.com/alparslanu6347/roman-numerals_terraform/main/app.py
 python3 app.py
-
 
 
