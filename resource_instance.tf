2d16667e54df7adb26c8f9ac5aabdc0ac820c0ac
arrow <alparslanu6347@gmail.com>
2023-10-17 21:50:05 +0300

    updated

--- resource_instance.tf
+++ resource_instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "arrow_roman-numerals_ec2" {
   vpc_security_group_ids = [ aws_security_group.arrow.id ]
   associate_public_ip_address = var.enable_public_ip
   subnet_id = "subnet-069d7f45d2659c70c" ## us-east-1a
-  user_data = file("${path.module}/arrow_userdata.sh") 
+  user_data = file("${path.module}/userdata.sh") 
   tags = {
     Name = var.instance_name
   }
