b7257d1f2810a0323cd4e06f903b710ea4346aaf
alparslanu634i7 <alparslanu6347@gmail.com>
2023-12-28 09:12:24 +0300

    project files updated

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_publicip" {  
-  description = "EC2 Instance Public IP"
-  value = aws_instance.arrow_roman-numerals_ec2.public_ip  
-}
-
-output "instance_publicdns" {     
-  description = "EC2 Instance Public DNS"
-  value = aws_instance.arrow_roman-numerals_ec2.public_dns
-}
